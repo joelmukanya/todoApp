dc32e13612b5f3d29a9700086782b0d0123e6f4e
joelmukanya <joel@lifechoices.co.za>
2022-06-19 12:36:17 +0200

    Left with one bug to fix

--- script/code.js
+++ script/code.js
@@ -2,45 +2,38 @@
 Load data from the local storage or provide a default data set with at least one record. 
 And save it to an array called "lists".
 */
+// localStorage.removeItem('items');
 let lists = JSON.parse( localStorage.getItem('items')) ? 
 JSON.parse( localStorage.getItem('items')) : [
     {
         id: 1,
         item: 'TV Stand',
-        createdDate: new Date().getDate()
+        createdDate: new Date()
     }
 ];
 // Add item
-function addItems(e) {
-    // Prevent the form to submit
-    e.preventDefault();
-    try{
-        let list = document.querySelector('#list-content').textContent;
-        // Fetch the last index of id
-        let index = parseInt(lists.map((item)=> {
-            return item.id;
-        })) + 1;
-        // Add a new item
-        lists.push(
-            {
-                id: index !== undefined ? index : 1 , 
-                item: list,
-                createdDate: new Date().getDate()
-            }
-        )
-        // Save new data to the localstorage
-        localStorage.setItem('items', JSON.stringify(lists));
-        // Load data from localStorage
-        loadFromLocalStorage();
-    }catch(e) {
-        console.error(e.message);
-    }
+function addItems() {
+    let list = document.getElementById('list-content').innerText;
+    console.log(list)
+    // Fetch the last index of id
+    let index = parseInt(lists.map((item)=> {
+        return item.id;
+    })) + 1;
+    // Add a new item
+    lists.push(
+        {
+            id: index !== undefined ? index : 1 , 
+            item: list,
+            createdDate: new Date()
+        }
+    );
+    // Save new data to the localstorage
+    localStorage.setItem('items', JSON.stringify(lists));
 }
 // Load data
 function loadFromLocalStorage() {
-    let data = JSON.parse(localStorage.getItem('items'));
-    console.log(data);
-    data.forEach( (item)=> {
+    console.log(lists);
+    lists.forEach( (item)=> {
         document.querySelector('#item-wrapper').innerHTML += 
         `
         <li class="bg-gradient list-unstyled">
@@ -51,19 +44,11 @@ function loadFromLocalStorage() {
         <i class="bi bi-x-octagon-fill list-icon"></i>
         </li>
         `;
-    } )
+    } );
 }
+
+// Load data
+loadFromLocalStorage();
 // btnAddItem
 const btnAddItem = document.querySelector('#addItem');
 btnAddItem.addEventListener('click', addItems);
-
-
-/*
-<li class="bg-gradient list-unstyled">
-    <input type="checkbox" class="chkItem form-check-input" id="chkItem" >
-    <span class="list-content">
-        
-    </span>
-    <i class="bi bi-x-octagon-fill list-icon"></i>
-</li>
-*/
